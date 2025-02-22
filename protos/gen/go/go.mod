4f08444d56fa3ccfa3bce2b577f4c933883a092a
Tadjaur <tadjaur@tas-groups.com>
2025-02-22 10:34:19 +0100

    fix generated file

--- protos/gen/go/go.mod
+++ protos/gen/go/go.mod
@@ -1,8 +1,6 @@
 module github.com/karibu-cap/sabitou/protos/gen/go
 
-go 1.21
-
-toolchain go1.24.0
+go 1.24.0
 
 require (
 	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.5-20250219170025-d39267d9df8f.1
--- protos/scripts/generate.sh
+++ protos/scripts/generate.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 set -e
 
+# Remove old generated files.
+rm -rf gen
+
 # Generate the protos
 buf generate
 
 # Merge generated OpenAPI specs.
 find gen/openapi/specs -type f -name "*.yaml" -print0 \
-| xargs -0 yq eval-all '. as $item ireduce ([]; . + $item )' \
+| sort -z \
+| xargs -0 yq eval-all '. as $item ireduce ({}; . * $item )' \
 > gen/openapi/openapi.merged.yaml
+
+# Add Dart pubspec.yaml file.
+cd gen/dart
+cat <<EOF > pubspec.yaml
+name: sabitou_rpc
+description: The sabitou dart rpc client.
+repository: https://github.com/karibu-cap/sabitou
+
+version: 1.0.0+1
+
+environment:
+  sdk: ^3.5.3
+
+dependencies:
+  connectrpc: ^0.3.0
+
+dev_dependencies:
+
+EOF
+
+dart pub get
+cd -
+
+# Add Go mod file.
+cd gen/go
+cat <<EOF > go.mod
+module github.com/karibu-cap/sabitou/protos/gen/go
+
+go 1.24.0
+
+EOF
+
+go mod tidy
+cd -
