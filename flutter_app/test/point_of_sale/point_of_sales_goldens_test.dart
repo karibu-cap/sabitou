8470b108b0f09912906087209e79d00afe745a94
warrenrhodes <webanalyse237@gmail.com>
2025-09-16 15:03:39 +0100

    Update the goldens

---
+++ flutter_app/test/point_of_sale/point_of_sales_goldens_test.dart
@@ -0,0 +1,37 @@
+import 'package:clock/clock.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:sabitou_clients/screens/point_of_sales/point_of_sales_view.dart';
+import 'package:sabitou_clients/services/rpc/connect_rpc.dart';
+import 'package:sabitou_clients/services/rpc/fake_transport.dart';
+import 'package:sabitou_clients/services/storage/app_storage.dart';
+
+import '../goldens.dart';
+
+void main() {
+  group('Goldens', () {
+    setUpAll(() {
+      final storage = AppStorageService(AppStorageType.fake, fakeStorage);
+      GetIt.I.registerSingletonIfAbsent<AppStorageService>(() => storage);
+    });
+
+    setUp(() {
+      GetIt.I.registerSingletonIfAbsent<ConnectRPCService>(
+        () => ConnectRPCService.new(clientChannel: fakeTransport),
+      );
+    });
+    testGoldens('Point of sales view', (tester) async {
+      return withClock(
+        Clock.fixed(DateTime.parse('2025-09-01 15:30:00Z').toUtc()),
+        () async {
+          await multiScreenMultiLocaleGolden(
+            tester,
+            const PointOfSalesView(),
+            'point_of_sales_view',
+          );
+        },
+      );
+    });
+  });
+}
