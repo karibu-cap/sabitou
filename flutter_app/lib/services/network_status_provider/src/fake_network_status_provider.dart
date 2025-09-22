b20d766691b00f84663bf32ffd77cca930de1164
nidovic <feutseludovic@gmail.com>
2025-09-22 21:18:20 +0100

    Fix network connection checker.

--- flutter_app/lib/services/network_status_provider/src/fake_network_status_provider.dart
+++ flutter_app/lib/services/network_status_provider/src/fake_network_status_provider.dart
@@ -6,39 +6,14 @@ class FakeNetworkStatusProvider implements NetworkStatusProvider {
   /// The initial status.
   final bool initialStatus;
 
-  /// The timer used for polling.
-  Timer? _timer;
-
-  /// The stream controller.
-  final StreamController<bool> _controller = StreamController.broadcast();
-
-  /// The current status.
-  final bool _current;
-
   @override
-  Stream<bool> get connectivityStream => _controller.stream;
+  Stream<bool> get connectivityStream => Stream<bool>.value(initialStatus);
 
   /// Creates a new instance of the fake network status provider.
-  FakeNetworkStatusProvider({this.initialStatus = true})
-    : _current = initialStatus;
+  FakeNetworkStatusProvider({this.initialStatus = true});
 
   @override
   Future<bool> checkConnectivity() async {
-    return _current;
-  }
-
-  @override
-  void startPolling() {
-    stopPolling();
-    _controller.add(_current);
-  }
-
-  @override
-  void stopPolling() => _timer?.cancel();
-
-  @override
-  void dispose() {
-    stopPolling();
-    _controller.close();
+    return initialStatus;
   }
 }
