455bdfa93d7f4edd88b0e0718ce30fe21a3cf965
warren rhodes <webanalyse237@gmail.com>
2025-09-22 20:46:32 +0100

    Fix inventory (#105)
    * Fix inventory
    
    * Update the goldens

--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import isar_community_flutter_libs
 import mobile_scanner
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
