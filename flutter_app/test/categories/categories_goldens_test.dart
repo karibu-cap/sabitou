f4773704ff9626323d1fbedda24ffb12a1f85093
nidovic <feutseludovic@gmail.com>
2025-09-27 11:42:21 +0100

    Fix some tests.

--- flutter_app/test/categories/categories_goldens_test.dart
+++ flutter_app/test/categories/categories_goldens_test.dart
@@ -14,18 +14,21 @@ final categoryFakeTransport = FakeTransportBuilder()
         ..categories.addAll([
           Category()
             ..refId = 'category_1'
+            ..name = Internationalized()
             ..name.en = 'New Category'
             ..name.fr = 'Nouvelle Catégorie'
             ..type = CategoryType.CATEGORY_TYPE_PRODUCT
             ..status = CategoryStatus.CATEGORY_STATUS_ACTIVE,
           Category()
             ..refId = 'category_2'
+            ..name = Internationalized()
             ..name.en = 'New Category 2'
             ..name.fr = 'Nouvelle Catégorie 2'
             ..type = CategoryType.CATEGORY_TYPE_PRODUCT
             ..status = CategoryStatus.CATEGORY_STATUS_INACTIVE,
           Category()
             ..refId = 'category_3'
+            ..name = Internationalized()
             ..name.en = 'New Category 3'
             ..name.fr = 'Nouvelle Catégorie 3'
             ..type = CategoryType.CATEGORY_TYPE_PRODUCT
@@ -33,13 +36,13 @@ final categoryFakeTransport = FakeTransportBuilder()
         ]);
     })
     .unary(CategoryService.createCategory, (req, _) async {
-      return CreateCategoryResponse();
+      return CreateCategoryResponse()..success = true;
     })
     .unary(CategoryService.updateCategory, (req, _) async {
-      return UpdateCategoryResponse();
+      return UpdateCategoryResponse()..success = true;
     })
     .unary(CategoryService.deleteCategory, (req, _) async {
-      return DeleteCategoryResponse();
+      return DeleteCategoryResponse()..success = true;
     })
     .build();
 
